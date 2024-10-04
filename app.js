74e646d6ba6d26fc0acbddfaa3bb4fbe45a0649e
joelvshimself <76189923+joelvshimself@users.noreply.github.com>
2024-10-03 21:03:10 -0600

    hola dios
    yo de nuevo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5500;
 // Permitir CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
@@ -15,15 +15,16 @@ app.use((req, res, next) => {
     next();
 });
 
+// Middleware para manejar JSON
+app.use(express.json());
 
-// Servir archivos estáticos desde la carpeta actual
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Ruta para servir el archivo HTML
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
 
 // Ruta para obtener usuarios y manejar CORS
 app.get('/api/usuarios', async (req, res) => {
@@ -35,6 +36,43 @@ app.get('/api/usuarios', async (req, res) => {
     } catch (error) {
         console.error('Error al obtener los datos:', error);
         res.status(500).json({ error: 'Error al obtener los datos' });
+    }
+});
+
+// Ruta para modificar el permiso de un usuario
+app.put('/api/usuarios/:id/modificar-permiso', async (req, res) => {
+    const userId = req.params.id;
+    const nuevoPermiso = req.body.permiso;
+
+    try {
+        const response = await fetch(`https://bufetecapi.onrender.com/usuarios/${userId}/modificar-permiso`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ permiso: nuevoPermiso })
+        });
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error al modificar el permiso:', error);
+        res.status(500).json({ error: 'Error al modificar el permiso' });
+    }
+});
+
+// Ruta para borrar un usuario
+app.delete('/api/usuarios/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const response = await fetch(`https://bufetecapi.onrender.com/usuarios/${userId}`, {
+            method: 'DELETE'
+        });
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error('Error al borrar el usuario:', error);
+        res.status(500).json({ error: 'Error al borrar el usuario' });
     }
 });
 
